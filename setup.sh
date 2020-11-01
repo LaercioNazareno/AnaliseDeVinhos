2780dcdbef2dca76636415bac3ef29bb9f3f9be4
LaercioNazareno <laercio.nazareno1@gmail.com>
2020-11-01 20:15:32 -0300

    commit inicial

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir -p ~ / .streamlit / 
+echo "\ 
+[geral] \ n \ 
+email = \" laercio.nazareno1@gmail.com \ "\ n \ 
+"> ~ / .streamlit / credentials.toml 
+echo "\ 
+[servidor] \ n \ 
+headless = true \ n \ 
+enableCORS = false \ n \ 
+port = $ PORT \ n \
